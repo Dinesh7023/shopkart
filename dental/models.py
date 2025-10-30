7ee587b89486f6cf3214a06973dde5c1d9516fb7
Dinesh7023 <dk68sdk@outlook.com>
2025-10-30 05:52:45 +0530

    new files added and modified

--- dental/models.py
+++ dental/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class customers(models.Model):
+    name = models.CharField(max_length=150, null=False, blank=False)
+    email = models.CharField(max_length=150, null=False, blank=False)
+    phone = models.IntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return self.name
