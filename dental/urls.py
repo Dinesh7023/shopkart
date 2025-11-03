d8162e97fdae9518412c234d60ecb02ef7a03e4f
Dinesh7023 <dk68sdk@outlook.com>
2025-11-03 07:02:54 +0530

    files are modified

--- dental/urls.py
+++ dental/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('homee', views.home, name="home"),
+    path('homee', views.home, name="homee"),
     path('service', views.service, name="service")
 ]
