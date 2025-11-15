b702cc8b4fa8244ca2eb7d264e2a8092db4a7965
Dinesh7023 <dk68sdk@outlook.com>
2025-11-15 21:07:37 +0530

    modified

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.home, name="home"),
     path('register',views.register, name="register"),
-    path('collections', views.collections, name="collections")
+    path('collections', views.collections, name="collections"),
+    path('collections/<str:name>', views.collectionsview, name="collections"),
+    path('collections/<str:cname>/<str:pname>', views.product_details, name="product_details")
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from http.client import HTTPResponse
+from django.shortcuts import redirect, render
 from . models import *
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -9,3 +11,25 @@ def register(request):
 def collections(request):
     category = Category.objects.filter(status = 0)
     return render(request, "shop/collections.html", {"category":category})
+
+def collectionsview(request,name):
+    if(Category.objects.filter(name = name, status = 0)):
+        products = Product.objects.filter(category__name = name)
+        return render(request, "shop/products/index.html", {"products":products, "category_name":name})
+    else:
+        messages.warning(request,"No such Category Found")
+        return redirect('collections')
+
+def product_details(request,cname,pname):
+    if(Category.objects.filter(name=cname, status=0)):
+        if(Product.objects.filter(name=pname, status=0)):
+            products = Product.objects.filter(name=pname, status=0).first()
+            return render(request, "shop/products/product_details.html",{"products":products})
+        else:
+            messages.error(request,"No Such Product Found")
+            return redirect('collections')
+    else:
+        messages.error(request,"No such Category Found")
+        return redirect('collections')
+
+    
