b0d4704fa0d6058debb212cf0d93f9cc368e0597
Dinesh7023 <dk68sdk@outlook.com>
2025-11-27 20:00:57 +0530

    added

--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render
 from . models import *
 from django.contrib import messages
 from shop.form import CustomUserForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def home(request):
@@ -12,8 +13,29 @@ def home(request):
    }
     return render(request, "shop/index.html", context)
 
-def login(request):
-    return render(request,"shop/login.html")
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect("/")
+    else:
+        if request.method=='POST':
+            name = request.POST.get('username')
+            pwd = request.POST.get('password')
+            user = authenticate(request, username=name, password=pwd)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "Logged in Successfully")
+                return redirect("/")
+            else:
+                messages.error(request, "Invalid User Name or Password")
+                return redirect("/login")
+        return render(request,"shop/login.html")
+
+    
+def logout_page(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request,"Logged Out Successfully")
+    return redirect("/")
 
 def register(request):
     form=CustomUserForm()
