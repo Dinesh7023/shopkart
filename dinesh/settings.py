7a52153c5e85e0f3aa852ec8b900127e4e68170f
Dinesh7023 <dk68sdk@outlook.com>
2026-01-08 20:15:03 +0530

    database url added

--- dinesh/settings.py
+++ dinesh/settings.py
@@ -93,10 +93,11 @@ WSGI_APPLICATION = 'dinesh.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
